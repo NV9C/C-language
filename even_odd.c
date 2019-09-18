1fc64bbd29108e9ed34ed95a33b597255ab54bee
NV9C <55490325+NV9C@users.noreply.github.com>
2019-09-18 17:09:08 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,20 @@
+/*Programe to given number is even or odd
+Name=Ravindra Yadav
+Date=26/08/2019*/
+#include<stdio.h>
+int main()
+{
+int num1,even,odd;
+printf("enter a number");
+scanf("%d",&num1);
+if(num1%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+}
+
