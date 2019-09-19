eb779d5e5ed1725537c2fdce713f5822f58725d8
NV9C <55490325+NV9C@users.noreply.github.com>
2019-09-19 07:15:17 -0700

    Add files via upload

---
+++ CIRCLE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+void main()
+{
+int r;
+float Area,Perimeter;
+printf("Enter Radius");
+scanf("%d",&r);
+Area=pi*r*r;
+Area("%d",&Area);
+Perimeter=2*pi*r;
+printf("%d",&Perimeter);
+getch();
+}
+
