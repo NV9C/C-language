df0a56c39a1fb6eafa9793a356099e8ced01443a
NV9C <55490325+NV9C@users.noreply.github.com>
2019-09-24 17:16:59 +0530

    Add files via upload

---
+++ EQUAL.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y;
+clrscr();
+printf("enter the number");
+scanf("%d %d",&x,&y);
+if(x==y)
+{
+printf("number are equal");
+}
+else
+{
+printf("number are not equal");
+}
+getch();
+}
